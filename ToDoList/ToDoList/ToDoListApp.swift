1d5ba93a5222888d82c9a11c24e3a8d2f690ce85
mand32128 <kojimandokoro1228@gmail.com>
2022-01-20 20:09:52 +0900

    create project

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Koji Mandokoro on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
